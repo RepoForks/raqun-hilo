26dae252b46d0da0ee408b76abeb3ac1f35be172
tyln <taylan.sbrcn@gmail.com>
2017-05-22 22:03:38 +0300

    enum name refactored

--- app/src/main/kotlin/com/raqun/hilo/data/Card.kt
+++ app/src/main/kotlin/com/raqun/hilo/data/Card.kt
@@ -1,8 +1,6 @@
 package com.raqun.hilo.data
 
-import com.raqun.hilo.data.Colors
-
 /**
  * Created by tyln on 22/05/2017.
  */
-data class Card(var num: Int, var color: Colors)
+data class Card(var num: Int, var color: Color)
--- app/src/main/kotlin/com/raqun/hilo/data/Color.kt
+++ app/src/main/kotlin/com/raqun/hilo/data/Color.kt
@@ -3,7 +3,7 @@ package com.raqun.hilo.data
 /**
  * Created by tyln on 22/05/2017.
  */
-enum class Colors(val type: Int) {
+enum class Color(val type: Int) {
     SPADES(0),
     HEARTS(1),
     DIAMONDS(2),
