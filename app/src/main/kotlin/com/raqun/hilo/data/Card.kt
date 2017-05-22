77aabd2d7b592e847df2adaa892fc52477845e55
tyln <taylan.sbrcn@gmail.com>
2017-05-22 21:57:02 +0300

    added kotlin data class and enum example

---
+++ app/src/main/kotlin/com/raqun/hilo/data/Card.kt
@@ -0,0 +1,7 @@
+package com.raqun.hilo.data
+
+/**
+ * Created by tyln on 22/05/2017.
+ */
+class Card {
+}
---
+++ app/src/main/kotlin/com/raqun/hilo/data/Colors.kt
@@ -0,0 +1,10 @@
+package com.raqun.hilo
+
+/**
+ * Created by tyln on 22/05/2017.
+ */
+enum class Colors(val type: Int) {
+    SPADES(0),
+    HEARTS(1),
+    
+}
